7d848c6759c8d7e548b78a9048131fe524ed00ab
Erik <erikdesjardins@users.noreply.github.com>
2015-07-12 16:31:12 -0400

    remove tabs when messages timeout fixes #12

--- src/background/main.js
+++ src/background/main.js
@@ -11,29 +11,39 @@
 		TabMgr.updatePlayState(tabId, data);
 	});
 
+	function removeTab(tabId) {
+		return function() {
+			TabMgr.remove(tabId);
+		};
+	}
+
 	Commands.addListener(Const.cmd.PLAY_PAUSE, function() {
 		TabMgr.first(function(tabId, tab) {
 			var messageName = tab.isPlaying ? Const.msg.PAUSE : Const.msg.PLAY;
-			Messages.send(messageName, tabId);
+			Messages.send(messageName, tabId)
+				.catch(removeTab(tabId));
 		});
 	});
 
 	Commands.addListener(Const.cmd.NEXT, function() {
 		TabMgr.first(function(tabId) {
-			Messages.send(Const.msg.NEXT, tabId);
+			Messages.send(Const.msg.NEXT, tabId)
+				.catch(removeTab(tabId));
 		});
 	});
 
 	Commands.addListener(Const.cmd.PREV, function() {
 		TabMgr.first(function(tabId) {
-			Messages.send(Const.msg.PREV, tabId);
+			Messages.send(Const.msg.PREV, tabId)
+				.catch(removeTab(tabId));
 		});
 	});
 
 	Commands.addListener(Const.cmd.STOP, function() {
 		TabMgr.each(function(tabId, tab) {
 			if (tab.isPlaying) {
-				Messages.send(Const.msg.PAUSE, tabId);
+				Messages.send(Const.msg.PAUSE, tabId)
+					.catch(removeTab(tabId));
 			}
 		});
 	});
