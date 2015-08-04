42e2ed8cde73c63fd573eb21eda1c99b879f6b3c
Erik <erikdesjardins@users.noreply.github.com>
2015-08-04 18:07:14 -0400

    wrapping Const in a closure is silly

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,31 +1,29 @@
-var Const = (() => {
-	return {
-		msg: {
-			REGISTER: 'tab-register',
-			UNREGISTER: 'tab-unregister',
-			PLAY_STATE: 'tab-playstate',
+var Const = {
+	msg: {
+		REGISTER: 'tab-register',
+		UNREGISTER: 'tab-unregister',
+		PLAY_STATE: 'tab-playstate',
 
-			INFO: 'tab-info',
-			ACTIONS: 'tab-actions',
-			DO_ACTION: 'tab-do-action',
-			FOCUS_TAB: 'tab-focus',
+		INFO: 'tab-info',
+		ACTIONS: 'tab-actions',
+		DO_ACTION: 'tab-do-action',
+		FOCUS_TAB: 'tab-focus',
 
-			PLAY_PAUSE: 'tab-play-pause',
-			NEXT: 'tab-next',
-			PREV: 'tab-prev',
+		PLAY_PAUSE: 'tab-play-pause',
+		NEXT: 'tab-next',
+		PREV: 'tab-prev',
 
-			ECHO: 'echo'
-		},
+		ECHO: 'echo'
+	},
 
-		cmd: {
-			PLAY_PAUSE: 'media-play-pause',
-			NEXT: 'media-next',
-			PREV: 'media-prev',
-			STOP: 'media-stop'
-		},
+	cmd: {
+		PLAY_PAUSE: 'media-play-pause',
+		NEXT: 'media-next',
+		PREV: 'media-prev',
+		STOP: 'media-stop'
+	},
 
-		storage: {
-			TABS: 'registered-tabs'
-		}
+	storage: {
+		TABS: 'registered-tabs'
 	}
-})();
+};
