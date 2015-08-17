b6efdeaa49d46a6348767fc65847d14d1e732dc0
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2015-08-17 17:32:37 -0400

    add jsdoc documentation

--- src/js/modules/background/storage.js
+++ src/js/modules/background/storage.js
@@ -1,6 +1,18 @@
+/**
+ * @file A simple <tt>Promise</tt>-based wrapper around <tt>chrome.storage.local</tt>.
+ * @module background/storage
+ */
+
 /* global chrome */
 import { isRefType } from '../util';
 
+/**
+ * Wraps <tt>chrome.storage.local.set</tt>.
+ * @param {string} key
+ * @param {*} value
+ * @returns {Promise<void, Error>} Rejects if <tt>chrome.runtime.lastError</tt> is set,
+ * resolves otherwise.
+ */
 export function set(key, value) {
 	const obj = { [key]: value };
 	return new Promise((resolve, reject) => {
@@ -10,6 +22,15 @@ export function set(key, value) {
 	});
 }
 
+/**
+ * Wraps <tt>chrome.storage.local.get</tt>.
+ * @template T
+ * @param {string} key
+ * @param {T} defaultValue
+ * @returns {Promise<T, Error>} Rejects if <tt>chrome.runtime.lastError</tt> is set,
+ * resolves with <tt>defaultValue</tt> if <tt>key</tt> does not exist in storage,
+ * resolves with the value fetched from storage otherwise.
+ */
 export function get(key, defaultValue) {
 	return new Promise((resolve, reject) => {
 		chrome.storage.local.get(key, items => {
@@ -26,13 +47,26 @@ export function get(key, defaultValue) {
 
 const hasListener = {};
 
-async function getAndSetOnSuspend(key, defaultValue) {
+/**
+ * Fetches a stored reference type with {@link get}, and stores it during <tt>chrome.runtime.onSuspend</tt> with {@link set}.
+ * Persistence cannot be guaranteed.
+ * @template T
+ * @param {string} key
+ * @param {T} defaultValue
+ * @returns {Promise<T, Error>} Rejects if it has been previously called with the same <tt>key</tt>,
+ * rejects if {@link get} would reject if called with the same arguments,
+ * rejects if the value resolved from {@link get} is not a reference type,
+ * resolves with the value resolved from {@link get} otherwise.
+ */
+export async function autopersist(key, defaultValue) {
+	if (hasListener[key]) {
+		throw new Error(`Key: ${key}, has been previously fetched with autopersist.`);
+	}
+
 	const val = await get(key, defaultValue);
 
 	if (!isRefType(val)) {
 		throw new TypeError(`Key: ${key}, value: ${val}, is not a reference type - changes cannot be persisted.`);
-	} else if (hasListener[key]) {
-		throw new Error(`Key: ${key}, has been previously fetched with autopersist.`);
 	}
 
 	chrome.runtime.onSuspend.addListener(() => set(key, val));
@@ -40,5 +74,3 @@ async function getAndSetOnSuspend(key, defaultValue) {
 
 	return val;
 }
-
-export { getAndSetOnSuspend as autopersist };
--- src/js/modules/content/messages.js
+++ src/js/modules/content/messages.js
@@ -1,26 +1,49 @@
+/**
+ * @file A wrapper around the <tt>sendMessage</tt> and <tt>onMessage</tt> APIs,
+ * supporting message types and optionally asynchronous handlers.
+ * @module content/messages
+ */
+
 /* global chrome */
 import { isPromise } from '../util';
 
 const listeners = {};
 
-export function addListener(messageType, callback) {
-	if (messageType in listeners) {
-		throw new Error(`Listener for message type: ${messageType} already exists.`);
+/**
+ * Register a listener to be invoked whenever a message of <tt>type</tt> is received.
+ * Responses may be sent synchronously or asynchronously, depending on the return value of <tt>callback</tt>.
+ * @template T
+ * @param {string} type
+ * @param {function(*): (T|Promise<T, *>)} callback Accepts the message data.
+ * If <tt>callback</tt> returns a non-promise value, a response will be sent synchronously.
+ * If <tt>callback</tt> returns a <tt>Promise</tt>, a response will be sent asynchronously when it resolves.
+ * If it rejects, an invalid response will be sent to close the message channel.
+ * @throws {Error} If a listener for <tt>messageType</tt> already exists.
+ * @returns {void}
+ */
+export function addListener(type, callback) {
+	if (type in listeners) {
+		throw new Error(`Listener for message type: ${type} already exists.`);
 	}
-	listeners[messageType] = callback;
+	listeners[type] = callback;
 }
 
-function sendMessage(type, data) {
+/**
+ * Send a message to the background page and popup, if available.
+ * @param {string} type
+ * @param {*} data
+ * @returns {Promise<*, Error>} Rejects if an invalid response is received,
+ * resolves with the response data otherwise.
+ */
+export function send(type, data) {
 	const message = { type, data };
 
 	return new Promise((resolve, reject) => {
 		chrome.runtime.sendMessage(message, response =>
-				response ? resolve(response.data) : reject(new Error(`Received empty response from message type: ${type}`))
+				response ? resolve(response.data) : reject(new Error(`Received invalid response from message type: ${type}`))
 		);
 	});
 }
-
-export { sendMessage as send };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	const { type, data } = request;
