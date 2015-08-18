9f1914ac9ba207a56a8a187c3c7d62d0da2fd9a7
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2015-08-18 15:05:01 -0400

    jsdoc documentation for Domain

--- src/js/modules/content/Domain.js
+++ src/js/modules/content/Domain.js
@@ -1,28 +1,91 @@
+/**
+ * @file The base class which coordinates setting up communication with the background page and popup.
+ * Not an ideal use case for classes, but sufficient.
+ * @module content/Domain
+ */
+
 import * as Const from '../constants';
 import * as Util from '../util';
 import * as Messages from './messages';
 
 export default class Domain {
-	constructor() {
-		this.actionData = [];
-	}
+	constructor() {}
 
+	/**
+	 * Buttons for controlling play/pause, next, and prev track.
+	 * Dispatching a click event on these should result in the desired action.
+	 * Getter properties may be used.
+	 * @typedef {Object} ButtonDefinition
+	 * @property {!Element} play
+	 * @property {!Element} next
+	 * @property {!Element} prev
+	 */
+
+	/**
+	 * @abstract
+	 * @returns {!ButtonDefinition} The three main buttons.
+	 */
 	getButtons() {
 		throw new Error('getButtons() is not defined.');
 	}
 
-	setupPlayState(/* callback, playButton */) {
+	/**
+	 * @abstract
+	 * @param {function(boolean): void} callback Should be invoked when the play state changes,
+	 * and when the initial play state is known.
+	 * @param {!Element} playButton The <tt>play</tt> property of the {@link ButtonDefinition} returned by {@link getButtons}.
+	 * @returns {void}
+	 */
+	setupPlayState(callback, playButton) {
 		throw new Error('setupPlayState(callback, playButton) is not defined.');
 	}
 
-	setupInfo(/* callback */) {
+	/**
+	 * Information about the currently playing track.
+	 * @typedef {Object} InfoDefinition
+	 * @property {string} image A valid CSS &lt;image&gt;. Ideally 250px square. Usually the album art.
+	 * @property {string} title Usually the track name.
+	 * @property {string} subtitle Usually the artist or playlist info.
+	 */
+
+	/**
+	 * @abstract
+	 * @param {function(InfoDefinition): void} callback Should be invoked whenever info changes,
+	 * and when initial info is known.
+	 * @returns {void}
+	 */
+	setupInfo(callback) {
 		throw new Error('setupInfo(callback) is not defined.');
 	}
 
+	/**
+	 * A custom action supported by the <tt>Domain</tt>.
+	 * @typedef {Object} ActionDefinition
+	 * @property {string} icon A CSS-encoded character from Font Awesome.
+	 * @property {boolean} state Whether the action icon should appear selected.
+	 */
+
+	/**
+	 * @callback ActionInit
+	 * @param {function(ActionDefinition): void} callback Should be invoked whenever state of the action changes,
+	 * and when initial state is known.
+	 * @returns {function|Promise<function>} Will be called when the action is invoked.
+	 * If a <tt>Promise</tt> is returned, no action invocations will be accepted until the <tt>Promise</tt> is resolved.
+	 */
+
+	/**
+	 * @returns {ActionInit[]} Functions to set up custom actions.
+	 */
 	getActions() {
 		return [];
 	}
 
+	/**
+	 * @param {Promise<void, Error>} [waitFor] Initialization will be delayed until this <tt>Promise</tt> resolves.
+	 * @returns {Promise<void, Error>} Rejects if <tt>waitFor</tt> rejects,
+	 * rejects if {@link getButtons} does not return a valid play button,
+	 * resolves when initialization is complete otherwise.
+	 */
 	async go(waitFor) {
 		await waitFor;
 
@@ -45,12 +108,13 @@ export default class Domain {
 		this.setupPlayState(Util.debounce(state => Messages.send(Const.msg.PLAY_STATE, state), 50), buttons.play);
 		this.setupInfo(Util.debounce(info => Messages.send(Const.msg.INFO, info), 50));
 
+		const actionData = [];
 		const actions = this.getActions();
-		const sendActionUpdate = Util.debounce(() => Messages.send(Const.msg.ACTIONS, this.actionData), 50);
+		const sendActionUpdate = Util.debounce(() => Messages.send(Const.msg.ACTIONS, actionData), 50);
 
 		await Util.asyncMap(actions, (setup, i) =>
 				setup(data => {
-					this.actionData[i] = data;
+					actionData[i] = data;
 					sendActionUpdate();
 				})
 		);
