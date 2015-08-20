00d76e107370736916d57ba3a2ac164fe7cdf4ae
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2015-08-20 15:39:01 -0400

    cleanup background message listeners factor out common code, abstract the creation of command -> message listeners and restore the background page's echo listener

--- src/js/background.js
+++ src/js/background.js
@@ -5,6 +5,17 @@ import * as Messages from './modules/background/messages';
 import * as Commands from './modules/background/commands';
 import * as TabMgr from './modules/background/tabmanager';
 
+function getTabSender(messageType) {
+	return async (data) => {
+		const { tabId } = await TabMgr.first();
+		try {
+			return await Messages.send(messageType, tabId, data);
+		} catch (e) {
+			await TabMgr.remove(tabId);
+		}
+	};
+}
+
 function updateOrFetch(messageType, key) {
 	Messages.addListener(messageType, async (data, tabId) => {
 		if (tabId) { // From tab, update stored data
@@ -17,60 +28,45 @@ function updateOrFetch(messageType, key) {
 }
 
 function forwardToTab(messageType) {
-	Messages.addListener(messageType, async (data) => {
-		const { tabId } = await TabMgr.first();
-		return await Messages.send(messageType, tabId, data);
-	});
+	Messages.addListener(messageType, getTabSender(messageType));
 }
 
-function getTabSender(messageType) {
-	return async () => {
-		const { tabId } = await TabMgr.first();
-		try {
-			await Messages.send(messageType, tabId);
-		} catch (e) {
-			await TabMgr.remove(tabId);
-		}
-	};
+function commandToTab(commandType, messageType) {
+	Commands.addListener(commandType, getTabSender(messageType));
 }
 
 Messages.addListener(Const.msg.REGISTER, (data, tabId) => TabMgr.add(tabId));
 // Synchronous to ensure response is sent before unload
 Messages.addListener(Const.msg.UNREGISTER, (data, tabId) => { TabMgr.remove(tabId); });
 
+Messages.addListener(Const.msg.ECHO, data => data);
+
 updateOrFetch(Const.msg.PLAY_STATE, 'isPlaying');
 updateOrFetch(Const.msg.INFO, 'info');
 updateOrFetch(Const.msg.ACTIONS, 'actions');
 
 forwardToTab(Const.msg.DO_ACTION);
+forwardToTab(Const.msg.PLAY_PAUSE);
+forwardToTab(Const.msg.NEXT);
+forwardToTab(Const.msg.PREV);
 
-const playPause = getTabSender(Const.msg.PLAY_PAUSE);
-Commands.addListener(Const.cmd.PLAY_PAUSE, playPause);
-Messages.addListener(Const.msg.PLAY_PAUSE, playPause);
+commandToTab(Const.cmd.PLAY_PAUSE, Const.msg.PLAY_PAUSE);
+commandToTab(Const.cmd.NEXT, Const.msg.NEXT);
+commandToTab(Const.cmd.PREV, Const.msg.PREV);
 
-const next = getTabSender(Const.msg.NEXT);
-Commands.addListener(Const.cmd.NEXT, next);
-Messages.addListener(Const.msg.NEXT, next);
-
-const prev = getTabSender(Const.msg.PREV);
-Commands.addListener(Const.cmd.PREV, prev);
-Messages.addListener(Const.msg.PREV, prev);
-
-async function stop() {
-	await TabMgr.each(async ({ tabId, tab }) => {
-		if (tab.isPlaying) {
-			// Avoid promoting the tab when its state changes
-			tab.isPlaying = false;
-			try {
-				await Messages.send(Const.msg.PLAY_PAUSE, tabId);
-			} catch (e) {
-				await TabMgr.remove(tabId);
+Commands.addListener(Const.cmd.STOP, () =>
+		TabMgr.each(async ({ tabId, tab }) => {
+			if (tab.isPlaying) {
+				// Avoid promoting the tab when its state changes
+				tab.isPlaying = false;
+				try {
+					await Messages.send(Const.msg.PLAY_PAUSE, tabId);
+				} catch (e) {
+					await TabMgr.remove(tabId);
+				}
 			}
-		}
-	});
-}
-
-Commands.addListener(Const.cmd.STOP, stop);
+		})
+);
 
 Messages.addListener(Const.msg.FOCUS_TAB, async () => {
 	const { tabId } = await TabMgr.first();
