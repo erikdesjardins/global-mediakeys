997ae2d75b757828d697be62798b4bd0f0ee63a4
Erik <erikdesjardins@users.noreply.github.com>
2017-03-10 23:27:05 -0500

    move auth tokens to .travis.yml (encrypted)

--- deploy.js
+++ deploy.js
@@ -7,9 +7,9 @@ const path = require('path');
 const deploy = require('chrome-extension-deploy');
 
 deploy({
-	clientId: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
-	refreshToken: process.env.REFRESH_TOKEN,
+	clientId: process.env.CHROME_CLIENT_ID,
+	clientSecret: process.env.CHROME_CLIENT_SECRET,
+	refreshToken: process.env.CHROME_REFRESH_TOKEN,
 	id: 'hhingnpbfhkjnmfkghlihmghgnddojeb',
 	zip: fs.readFileSync(path.join(__dirname, 'dist/GMK.zip'))
 }).then(function() {
