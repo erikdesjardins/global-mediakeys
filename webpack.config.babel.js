f254b4e5bc381b91837e0ab5cb40567f86f13b2b
Erik <erikdesjardins@users.noreply.github.com>
2017-08-03 22:58:46 -0400

    don't emit comments in production

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -26,7 +26,7 @@ module.exports = {
 		}, {
 			test: /\.js$/,
 			use: [
-				{ loader: 'babel-loader' },
+				{ loader: 'babel-loader', options: { comments: !isProduction } },
 			],
 		}, {
 			test: /\.scss$/,
